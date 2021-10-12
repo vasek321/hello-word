d3ec50390dd679039d4a090c23e84e2467d3be72
vasek321 <91741841+vasek321@users.noreply.github.com>
2021-10-12 13:49:08 +0200

    Add files via upload

--- cviceni_2_1.cpp
+++ cviceni_2_1.cpp
@@ -1,5 +1,6 @@
 // my first program in C++
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
@@ -32,6 +33,26 @@ int main()
         cout << jmeno << " " << prijmeni << " je " << vek << " let stary a ma " << vyska << " cm" << endl;
         cout << "Nezname dosazene vzdelani." << endl;
     }
+
+    fstream file;
+    file.open("vystup.txt",ios::out);
+    if(!file)
+    {
+      return 0;
+    }
+    cout << "\nFile created successfully."<< endl;
+    file<<jmeno<<" "<<prijmeni<<" "<<vek<<" "<<vyska<<" "<<vzdelani<< endl;
+    file.close();
+
+    file.open("vystup.txt",ios::in);
+    if(!file){
+        return 0;
+    }
+    file>>jmeno;
+    file>>prijmeni;
+
+    cout << "jmeno: "<<jmeno<<", prijmeni: "<<prijmeni<<"\nvek: "<<vek<<"\nvyska: "<<vyska<<"\nvzdelani: "<<vzdelani<< endl;
+
 
 
     return 0;
